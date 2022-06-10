23513ea2c826964158eab72822efa217c82d6834
Yeheum Choi <yh.choi@purple-nest.com>
2022-06-10 18:11:45 +0900

    [ENH] : 초기 세팅 초기 세팅 완료

---
+++ src/hooks/useResize.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+export const useResize = () => {
+  const [containerHeight, setContainerHeight] = useState(0);
+  const [containerWidth, setContainerWidth] = useState(0);
+
+  useEffect(() => {
+    setContainerHeight(window.innerHeight);
+    setContainerWidth(window.innerWidth);
+
+    function resizeEvent() {
+      setContainerHeight(window.innerHeight ?? 0);
+      setContainerWidth(window.innerWidth ?? 0);
+    }
+    const onResize = () => {
+      window.requestAnimationFrame(resizeEvent);
+    };
+    window.addEventListener('resize', onResize);
+
+    return () => window.removeEventListener('resize', onResize);
+  }, []);
+
+  return { width: containerWidth, height: containerHeight };
+};
---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const _404 = () => {
+  return <div style={{ color: 'white' }}>This is error page</div>;
+};
+
+export default _404;
